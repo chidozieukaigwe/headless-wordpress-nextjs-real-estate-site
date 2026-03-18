77b200ee66fc2b222f5428333f912d51eec28369
chido <chidodesigns@gmail.com>
2026-03-18 11:26:41 +0000

    button cmp created and tailwind directive implemented

--- components/MainMenu/MainMenu.js
+++ components/MainMenu/MainMenu.js
@@ -1,8 +1,12 @@
-import { Main } from "next/document";
+import { ButtonLink } from "components/ButtonLink";
 import Link from "next/link";
 import { FaHouseUser, FaHeart } from "react-icons/fa";
 
-export const MainMenu = ({ items }) => {
+export const MainMenu = ({
+  items,
+  callToActionLabel,
+  callToActionDestination,
+}) => {
   console.log("Main Menu", items);
   return (
     <div className="bg-slate-800 text-white px-5 h-[64px] sticky top-0 z-20 flex">
@@ -36,6 +40,14 @@ export const MainMenu = ({ items }) => {
             )}
           </div>
         ))}
+        {callToActionLabel && callToActionDestination && (
+          <div className="ml-3 my-auto">
+            <ButtonLink
+              destination={callToActionDestination}
+              label={callToActionLabel}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
