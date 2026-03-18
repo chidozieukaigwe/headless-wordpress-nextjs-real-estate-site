21a9a39940157c1d7aadbd93b07600507b986fc4
chido <chidodesigns@gmail.com>
2026-03-18 23:20:42 +0000

    rendering search results

--- components/PropertySearch/PropertySearch.js
+++ components/PropertySearch/PropertySearch.js
@@ -1,13 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Results } from "./Results";
 
 export const PropertySearch = () => {
+  const [properties, setProperties] = useState([]);
   useEffect(() => {
     const search = async () => {
       const response = await fetch("/api/search").then((res) => res.json());
       const data = await response;
-      console.log("DATA", data);
+      console.log("Properties Data", data.properties.nodes);
+      setProperties(data.properties.nodes);
     };
     search();
   }, []);
-  return <div>Property Search</div>;
+  return <Results properties={properties} />;
 };
