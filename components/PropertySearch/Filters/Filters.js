cd8d321c67aa727a9af6c3259e7837788c6008f3
chido <chidodesigns@gmail.com>
2026-03-19 09:12:47 +0000

    input cmps and filters added to all props page

---
+++ components/PropertySearch/Filters/Filters.js
@@ -0,0 +1,31 @@
+import { Input } from "components/Input";
+
+export const Filters = () => {
+  return (
+    <div className="max-w-5xl mx-auto my-5 flex gap border-slate-400 border-2 p-5 rounded-md">
+      <div className="flex-1">
+        <div>
+          <label className="cursor-pointer">
+            <input type="checkbox" className="mr-2" />
+            <span className="pl-2">has parking</span>
+          </label>
+        </div>
+        <div>
+          <label className="cursor-pointer">
+            <input type="checkbox" className="mr-2" />
+            <span className="pl-2">pet friendly</span>
+          </label>
+        </div>
+      </div>
+      <div className="flex-1">
+        <span>Min price</span>
+        <Input type="number" />
+      </div>
+      <div className="flex-1">
+        <span>Max price</span>
+        <Input type="number" />
+      </div>
+      <div className="btn">Search</div>
+    </div>
+  );
+};
--- components/PropertySearch/PropertySearch.js
+++ components/PropertySearch/PropertySearch.js
@@ -3,6 +3,7 @@ import { Results } from "./Results";
 import { Pagination } from "./Pagination /Pagination";
 import { useRouter } from "next/router";
 import queryString from "query-string";
+import { Filters } from "./Filters";
 
 export const PropertySearch = () => {
   const [properties, setProperties] = useState([]);
@@ -39,6 +40,7 @@ export const PropertySearch = () => {
 
   return (
     <div>
+      <Filters />
       <Results properties={properties} />
       <Pagination
         onPageClick={handlePageClick}
